a03d7ac8d4b2dae2d8afee3fc92483dd089a9a39
likelive <likelive@outlook.com>
2026-04-01 09:56:39 +0800

    Remove Test Framework command and fix typo

--- CS/App.cs
+++ CS/App.cs
@@ -86,7 +86,6 @@ namespace RevitLookup
       optionsBtn.AddPushButton( new PushButtonData( "Snoop Current Selection...", "Snoop Current Selection...", ExecutingAssemblyPath, "RevitLookup.CmdSnoopModScope" ) );
       optionsBtn.AddPushButton( new PushButtonData( "Snoop Active View...", "Snoop Active View...", ExecutingAssemblyPath, "RevitLookup.CmdSnoopActiveView" ) );
       optionsBtn.AddPushButton( new PushButtonData( "Snoop Application...", "Snoop Application...", ExecutingAssemblyPath, "RevitLookup.CmdSnoopApp" ) );
-      optionsBtn.AddPushButton( new PushButtonData( "Test Framework...", "Test Framework...", ExecutingAssemblyPath, "RevitLookup.CmdTestShell" ) );
     }
 
     private void AddAppDocEvents( ControlledApplication app )
